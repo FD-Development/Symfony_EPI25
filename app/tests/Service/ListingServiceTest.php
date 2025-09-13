3eb87138f19a3cbdd9e5e1aa1f345630cacae2be
Cave24 <filip.duber@gmail.com>
2025-09-13 13:56:20 +0200

    Test ListingService quality improvement

--- app/tests/Service/ListingServiceTest.php
+++ app/tests/Service/ListingServiceTest.php
@@ -1,35 +1,68 @@
 <?php
 
+/**
+ * Listing Service Test.
+ */
+
 namespace App\Tests\Service;
 
 use App\Entity\Listing;
 use App\Entity\Category;
 use App\Service\ListingServiceInterface;
-use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
  * @class ListingServiceTest.
  */
-class ListingServiceTest extends WebTestCase
+class ListingServiceTest extends KernelTestCase
 {
     /**
      * Tests the filtering by category of listings.
      */
     public function testListingsFilteredByCategory(): void
     {
+        // given
+        self::bootKernel();
         $container = static::getContainer();
-
         $listingService = $container->get(ListingServiceInterface::class);
-        $categoryRepository = $container->get(CategoryRepository::class);
+        $entityManager = $container->get('doctrine')->getManager();
 
-        $category = $categoryRepository->findOneBy(['title' => 'Test 1']);
-        $this->assertNotNull($category, 'Kategoria Test 1 musi istnieć. Czy Fixtures zostały załadowane do testowej bazy danych?');
+        // Category and Listing 1
+        $category1 = new Category();
+        $category1->setTitle('Test Category 1');
+        $entityManager->persist($category1);
 
-        // Act
-        $pagination = $listingService->getActivatedPaginatedListings(1, $category->getId());
+        for ($i = 0; $i < 5; ++$i) {
+            $listingCategory1 = new Listing();
+            $listingCategory1->setTitle('Listing Test 1 - '.($i + 1));
+            $listingCategory1->setDescription('Test description');
+            $listingCategory1->setCreatedAt(new \DateTimeImmutable());
+            $listingCategory1->setActivatedAt(null);
+            $listingCategory1->setCategory($category1);
 
-        // Assert
+            $entityManager->persist($listingCategory1);
+        }
+
+        // Category and Listing 2
+        $category2 = new Category();
+        $category2->setTitle('Test Category 2');
+        $entityManager->persist($category2);
+
+        $listingCategory2 = new Listing();
+        $listingCategory2->setTitle('Listing different category');
+        $listingCategory2->setDescription('Test description');
+        $listingCategory2->setCreatedAt(new \DateTimeImmutable());
+        $listingCategory2->setActivatedAt(null);
+        $listingCategory2->setCategory($category2);
+
+        $entityManager->persist($listingCategory2);
+
+        $entityManager->flush();
+
+        // when
+        $pagination = $listingService->getNonActivatedPaginatedListings(1, $category1->getId());
+
+        // then
         $titles = [];
         foreach ($pagination as $listing) {
             $titles[] = $listing->getTitle();
@@ -44,6 +77,12 @@ class ListingServiceTest extends WebTestCase
         ];
 
         $this->assertSame($expected, $titles, 'Powinny zwrócić się tylko ogłoszenia z kategorii Test 1.');
+
+        // cleanup
+        $entityManager->remove($listingCategory2);
+        $entityManager->remove($category1);
+        $entityManager->remove($category2);
+        $entityManager->flush();
     }
 
     /**
